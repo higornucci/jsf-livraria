2ce0ac202c0ca0fb99adce8f9845bd685be0c011
Higor Nucci <higornucci@gmail.com>
2017-08-21 16:05:23 -0400

    separando contextos

--- src/main/java/br/com/higornucci/loja/bean/LivroBean.java
+++ src/main/java/br/com/higornucci/loja/bean/LivroBean.java
@@ -1,48 +1,23 @@
 package br.com.higornucci.loja.bean;
 
+import br.com.higornucci.loja.model.Livro;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class LivroBean {
 
-    private String titulo;
-    private String isbn;
-    private double preco;
-    private String dataLancamento;
+    private Livro livro = new Livro();
 
     public void gravar() {
-        System.out.println("Gravando livro ");
+        System.out.println("Gravando livro " + livro.getTitulo());
     }
 
-    public String getTitulo() {
-        return titulo;
+    public Livro getLivro() {
+        return livro;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-    public String getDataLancamento() {
-        return dataLancamento;
-    }
-
-    public void setDataLancamento(String dataLancamento) {
-        this.dataLancamento = dataLancamento;
+    public void setLivro(Livro livro) {
+        this.livro = livro;
     }
 }
