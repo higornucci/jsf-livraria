ec753d1a74ffc0d2a3c25e4caf3f1d0451adf20a
Higor Nucci <higornucci@gmail.com>
2017-08-22 08:22:00 -0400

    inserindo dados do autor

--- src/main/java/br/com/higornucci/loja/bean/LivroBean.java
+++ src/main/java/br/com/higornucci/loja/bean/LivroBean.java
@@ -1,14 +1,20 @@
 package br.com.higornucci.loja.bean;
 
 import br.com.higornucci.loja.dao.DAO;
+import br.com.higornucci.loja.modelo.Autor;
 import br.com.higornucci.loja.modelo.Livro;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.List;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+
+	private Integer autorId;
 
 	public Livro getLivro() {
 		return livro;
@@ -22,6 +28,31 @@ public class LivroBean {
 		}
 
 		new DAO<>(Livro.class).adiciona(this.livro);
+		this.livro = new Livro();
 	}
 
+	public void gravarAutor() {
+		Autor autor = new DAO<>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+
+	public List<Livro> getLivros() {
+		return new DAO<>(Livro.class).listaTodos();
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<>(Autor.class).listaTodos();
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
 }
