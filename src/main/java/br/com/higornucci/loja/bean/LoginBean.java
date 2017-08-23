beece37262f550de73b9d0a76616967e5087b0c9
Higor Nucci <higornucci@gmail.com>
2017-08-23 09:23:02 -0400

    pode deslogar agora

--- src/main/java/br/com/higornucci/loja/bean/LoginBean.java
+++ src/main/java/br/com/higornucci/loja/bean/LoginBean.java
@@ -4,6 +4,7 @@ import br.com.higornucci.loja.dao.UsuarioDao;
 import br.com.higornucci.loja.modelo.Usuario;
 import br.com.higornucci.loja.util.RedirectView;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -26,10 +27,18 @@ public class LoginBean {
                     .put("usuarioLogado", this.usuario);
             return new RedirectView("livro");
         }
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        context.addMessage(null, new FacesMessage("Usuário não encontrado"));
         return new RedirectView("login");
     }
 
     public Usuario getUsuario() {
         return usuario;
+    }
+
+    public String deslogar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().remove("usuarioLogado");
+        return "login?faces-redirect=true";
     }
 }
